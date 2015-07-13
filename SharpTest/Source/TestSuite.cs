93e9ad36ec0fc4716a2a8df0b9f7679346296b4f
Stuart Farnaby <personal@stuartfarnaby.co.uk>
2015-07-13 21:59:00 +0100

    Updated TestSuite.cs to include full parsing of attribute data

--- SharpTest/Source/TestSuite.cs
+++ SharpTest/Source/TestSuite.cs
@@ -1,33 +1,38 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SharpTest
 {
-	public class TestSuite
+	public class TestSuite : IComparable<TestSuite>
 	{
 		private String name = null;
 		private String description = null;
 		private TestFormat format = TestFormat.Run;
 		private UInt32 order = 0;
 
-		public String Name
+		private List<Test> tests = new List<Test>();
+
+		internal String Name
 		{
 			get { return this.name; }
 			set { this.name = value; }
 		}
 
-		public String Description
+		internal String Description
 		{
 			get { return this.description; }
 			set { this.description = value; }
 		}
 
-		public TestFormat Format
+		internal TestFormat Format
 		{
 			get { return this.format; }
 			set { this.format = value; }
 		}
 			
-		public UInt32 Order
+		internal UInt32 Order
 		{
 			get { return this.order; }
 			set { this.order = value; }
@@ -43,9 +48,19 @@ namespace SharpTest
 
 		}
 
+		public virtual Task BeforeAllAsync()
+		{
+			return null;
+		}
+
 		public virtual void AfterAll()
 		{
 
+		}
+
+		public virtual Task AfterAllAsync()
+		{
+			return null;
 		}
 
 		public virtual void BeforeEach(Test test)
@@ -53,9 +68,75 @@ namespace SharpTest
 
 		}
 
+		public virtual Task BeforeEachAsync(Test test)
+		{
+			return null;
+		}
+
 		public virtual void AfterEach(Test test)
 		{
 
+		}
+
+		public virtual Task AfterEachAsync(Test test)
+		{
+			return null;
+		}
+
+		public int CompareTo(TestSuite suite)
+		{
+			return 0;
+		}
+
+		internal void Prepare()
+		{
+			ParseReflectiveProperties();
+			ParseTestSuiteAttribute();
+			PrepareTests();
+
+		}
+
+		private void ParseTestSuiteAttribute() 
+		{
+			TestAttribute testAttribute = (TestAttribute)Attribute.GetCustomAttribute(this.GetType(), typeof(TestAttribute));
+			if(testAttribute != null) 
+			{
+				Name = testAttribute.Name;
+				Description = testAttribute.Description;
+				Format = testAttribute.Format;
+				Order = testAttribute.Order;
+			}
+		}
+
+		private void ParseReflectiveProperties()
+		{
+			Name = this.GetType().Name;
+		}
+
+		private void PrepareTests()
+		{
+			//Get all the public methods linked to the test suite
+			MethodInfo[] methods = this.GetType().GetMethods();
+			foreach(MethodInfo method in methods) 
+			{
+				if(ShouldPrepareMethod(method)) 
+				{
+					tests.Add(PrepareTest(method));
+				}
+			}
+		}
+
+		private Boolean ShouldPrepareMethod(MethodInfo method)
+		{
+			HashSet<String> toIgnore = new HashSet<string>{"BeforeAll", "BeforeAllAsync", "AfterAll", "AfterAllAsync", "BeforeEach" , "BeforeEachAsync", "AfterEach", "AfterEachAsync", "CompareTo", "ToString", "GetHashCode", "GetType", "Equals"};
+			return !(toIgnore.Contains(method.Name));
+		}
+
+		private Test PrepareTest(MethodInfo method)
+		{
+			Test test = new Test(method, this);
+			test.Prepare();
+			return test;
 		}
 	}
 }
