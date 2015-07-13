40a05e576ce302220fabcacfeba4a2612e428f3e
Stuart Farnaby <personal@stuartfarnaby.co.uk>
2015-07-13 22:37:33 +0100

    Updated TestResult to have a time/status properties

--- SharpTest/Source/Results/TestResult.cs
+++ SharpTest/Source/Results/TestResult.cs
@@ -4,6 +4,21 @@ namespace SharpTest.Results
 {
 	public class TestResult
 	{
+		private UInt32 timeTaken = 0;
+		private TestStatus status = TestStatus.Pending;
+
+		public UInt32 TimeTaken
+		{
+			get { return this.timeTaken; }
+			internal set { this.timeTaken = value; }
+		}
+
+		public TestStatus Status
+		{
+			get { return this.status; }
+			internal set { this.status = value; }
+		}	
+
 		public TestResult()
 		{
 			
