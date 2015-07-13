e902dfa7cd88b6e7fb204713bc76a8ae1680c380
Stuart Farnaby <personal@stuartfarnaby.co.uk>
2015-07-14 00:48:13 +0100

    Added the Runnable and RunnableContainer to house reflective code

---
+++ SharpTest/Source/Internal/Runnable.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+
+namespace SharpTest.Internal
+{
+	public class Runnable : IComparable<Runnable>
+	{
+		private String name = null;
+		private String description = null;
+		private TestFormat format = TestFormat.Run;
+		private UInt32 order = 0;
+
+		public String Name
+		{
+			get { return this.name; }
+			internal set { this.name = value; }
+		}
+
+		public String Description
+		{
+			get { return this.description; }
+			internal set { this.description = value; }
+		}
+
+		internal TestFormat Format
+		{
+			get { return this.format; }
+			set { this.format = value; }
+		}
+
+		internal UInt32 Order
+		{
+			get { return this.order; }
+			set { this.order = value; }
+		}
+
+		public Runnable()
+		{
+			
+		}
+
+		public int CompareTo(Runnable runnable)
+		{
+			return this.Order.CompareTo(runnable.Order);
+		}
+
+		internal virtual void Prepare()
+		{
+			ParseReflectiveProperties();
+			ParseTestSuiteAttribute();
+		}
+
+		internal virtual void ParseReflectiveProperties()
+		{
+			Name = this.GetType().Name;
+		}
+
+		internal void ParseTestSuiteAttribute() 
+		{
+			TestAttribute testAttribute = (TestAttribute)Attribute.GetCustomAttribute(this.GetType(), typeof(TestAttribute));
+			if(testAttribute != null) 
+			{
+				if(testAttribute.Name != null)
+				{
+					Name = testAttribute.Name;
+				}
+
+				Description = testAttribute.Description;
+				Format = testAttribute.Format;
+				Order = testAttribute.Order;
+			}
+		}
+
+		internal String ParseName(String name)
+		{
+			StringBuilder builder = new StringBuilder();
+			foreach (char c in name) {
+				if(Char.IsUpper(c) && builder.Length > 0) 
+				{
+					builder.Append(' ');
+				}
+
+				builder.Append(c);
+			}
+
+			return builder.ToString();
+		}
+	}
+}
+
---
+++ SharpTest/Source/Internal/RunnableContainer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharpTest.Internal
+{
+	public class RunnableContainer : List<Runnable>
+	{
+		public RunnableContainer()
+		{
+			
+		}
+
+		public void Prepare()
+		{
+			//Iterate over each runnable and prepare it ready for testing
+			//and mark the 'only' options if required
+
+			Boolean only = false;
+			foreach(Runnable runnable in this) 
+			{
+				runnable.Prepare();
+				if(runnable.Format == TestFormat.Only)
+				{
+					only = true;
+				}
+			}
+
+			if(only)
+			{
+				RemoveNonOnly();
+			} 
+			else
+			{
+				RemoveSkipped();
+			}
+
+			this.Sort();
+		}
+
+		private void RemoveNonOnly()
+		{
+			for (var i = Count - 1; i >= 0; i--)
+			{ 
+				Runnable runnable = this[i];
+				if(runnable.Format != TestFormat.Only)
+				{
+					this.RemoveAt(i);
+				}
+			}
+		}
+
+		private void RemoveSkipped()
+		{
+			for (var i = Count - 1; i >= 0; i--)
+			{ 
+				Runnable runnable = this[i];
+				if(runnable.Format == TestFormat.Skip)
+				{
+					this.RemoveAt(i);
+				}
+			}
+		}
+	}
+}
+
