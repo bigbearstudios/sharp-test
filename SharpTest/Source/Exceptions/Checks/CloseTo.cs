a0795ae889615e6185c96212198c527c98f4888f
Stuart Farnaby <personal@stuartfarnaby.co.uk>
2015-07-20 09:43:25 +0100

    Added CloseTo Checks

---
+++ SharpTest/Source/Exceptions/Checks/CloseTo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SharpTest.Exceptions.Checks
+{
+	internal static class CloseTo
+	{
+		internal static void Check(Expect expect, int closeTo, int delta)
+		{
+			
+		}
+
+		internal static void Check(Expect expect, float closeTo, float delta)
+		{
+
+		}
+
+		internal static void Check(Expect expect, double closeTo, double delta)
+		{
+
+		}
+	}
+}
+
