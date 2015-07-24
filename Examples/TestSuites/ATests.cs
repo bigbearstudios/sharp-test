e672236d8370c34033d961672705781ea5209c5e
Stuart Farnaby <personal@stuartfarnaby.co.uk>
2015-07-24 11:21:36 +0100

    Added Tests for Contains, Length, Count and ThrowException

--- Examples/TestSuites/ATests.cs
+++ Examples/TestSuites/ATests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using SharpTest;
 using SharpTest.Exceptions;
@@ -7,10 +8,36 @@ namespace Examples
 {
 	public class ATests : TestSuite
 	{
+		private int[] emptyArray = new int[]{};
+		private int[] filledArray = new int[]{1,2,3,4,5};
+
+		private List<int> emptyList = new List<int>();
+		private List<int> filledList = new List<int>(){ 1, 2, 3, 4, 5 };
+
 		public void Length()
 		{
-			new Expect(new int[]{ }).To.Have.A.Length.EqualTo(0);
-			new Expect(new int[]{ 1, 2, 3, 4, 5 }).To.Have.A.Length.EqualTo(5);
+			new Expect(emptyArray).To.Have.A.Length.EqualTo(0);
+			new Expect(filledArray).To.Have.A.Length.EqualTo(5);
+			new Expect(filledArray).To.Have.A.Length.Above(4);
+			new Expect(filledArray).To.Have.A.Length.AtLeast(4);
+			new Expect(filledArray).To.Have.A.Length.Below(6);
+			new Expect(filledArray).To.Have.A.Length.Between(4, 6);
+			new Expect(filledArray).To.Have.A.Length.CloseTo(4, 2);
+			new Expect(filledArray).To.Have.A.Length.Of(5);
+			new Expect(filledArray).To.Have.A.Length.Within(4, 6);
+		}
+
+		public void Count()
+		{
+			new Expect(emptyList).To.Have.A.Count.EqualTo(0);
+			new Expect(filledList).To.Have.A.Count.EqualTo(5);
+			new Expect(filledList).To.Have.A.Count.Above(4);
+			new Expect(filledList).To.Have.A.Count.AtLeast(4);
+			new Expect(filledList).To.Have.A.Count.Below(6);
+			new Expect(filledList).To.Have.A.Count.Between(4, 6);
+			new Expect(filledList).To.Have.A.Count.CloseTo(4, 2);
+			new Expect(filledList).To.Have.A.Count.Of(5);
+			new Expect(filledList).To.Have.A.Count.Within(4, 6);
 		}
 	}
 }
--- Examples/TestSuites/ToTests.cs
+++ Examples/TestSuites/ToTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using SharpTest;
 using SharpTest.Exceptions;
@@ -26,6 +27,49 @@ namespace Examples
 			{
 
 			})).To.Not.ThrowException();
+		}
+
+		public void Exist()
+		{
+			new Expect(new object()).To.Exist();
+			new Expect(false).To.Exist();
+			new Expect(1.0).To.Exist();
+			new Expect(34).To.Exist();
+			new Expect(0).To.Exist();
+		}
+
+		public void NotExist()
+		{
+			new Expect(null).To.Not.Exist();
+		}
+
+		public void Contain()
+		{
+			new Expect("thisbeastring").To.Contain("beast");
+			new Expect(new List<int>{ 1, 2, 3, 4, 5 }).To.Contain(3);
+		}
+
+		public void ContainThrow()
+		{
+			new Expect((Action)(() =>
+			{
+				new Expect("thisbeastring").To.Contain("chinese");
+			})).To.ThrowException();
+		}
+
+		public void NotContain()
+		{
+			new Expect("thisbeastring").To.Not.Contain("chinese");
+			new Expect(new List<int>{ 1, 2, 3, 4, 5 }).To.Not.Contain(10);
+		}
+
+		public void NotContainThrow()
+		{
+			new Expect((Action)(() =>
+			{
+				new Expect("thisbeastring").To.Not.Contain("beast");
+				new Expect(new List<int>{ 1, 2, 3, 4, 5 }).To.Not.Contain(3);
+			})).To.ThrowException();
 		}
 	}
 }
