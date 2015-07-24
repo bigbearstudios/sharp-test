76f738db482b4d834f7d5aa4835fdbcee27c1a1a
Stuart Farnaby <personal@stuartfarnaby.co.uk>
2015-07-24 11:19:07 +0100

    Added Contains and Exists checks

---
+++ SharpTest/Source/Exceptions/Checks/Contains.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace SharpTest.Exceptions.Checks
+{
+	public static class Contains
+	{
+		internal static void Check(Expect expect, Object contains)
+		{
+			//Check to see if we can respond to 'contains' method
+			if(expect.TestObject != null)
+			{
+				try
+				{
+					Type type = expect.TestObject.GetType();
+					MethodInfo info = type.GetMethod("Contains");
+					if(info != null)
+					{
+						Boolean result = (Boolean)info.Invoke(expect.TestObject, new object[]{ contains });
+						if(result)
+						{
+							if(expect.Reversed)
+							{
+								throw new Exception(String.Format("The object {0} ({1}) did not contain {2}", expect.TestObject, expect.Name, contains));
+							}
+						}
+						else 
+						{
+							if(!expect.Reversed)
+							{
+								throw new Exception(String.Format("The object {0} ({1}) contained {2}", expect.TestObject, expect.Name, contains));
+							}
+						}
+					}
+					else
+					{
+						throw new Exception(String.Format("The object {0} ({1}) could not be be checked if it contains {2}", expect.TestObject, expect.Name, contains));
+					}
+				}
+				catch(AmbiguousMatchException ex)
+				{
+					ex.ToString();
+					throw new Exception(String.Format("The object {0} ({1}) could not be be checked if it contains {2}", expect.TestObject, expect.Name, contains));
+				}
+			}
+			else
+			{
+				throw new Exception(String.Format("The object {0} ({1}) was null and thus could not be be checked if it contains {2}", expect.TestObject, expect.Name, contains));
+			}
+		}
+	}
+}
+
---
+++ SharpTest/Source/Exceptions/Checks/Exists.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SharpTest.Exceptions.Checks
+{
+	internal static class Exists
+	{
+		internal static void Check(Expect expect)
+		{
+			if(expect.TestObject == null)
+			{
+				if(!expect.Reversed)
+				{
+					throw new Exception(String.Format("The object {0} ({1}) was not Ok", expect.TestObject, expect.Name));
+				}
+			}
+			else
+			{
+				Type type = expect.TestObject.GetType();
+
+				//Any value type is marked as 'existing'
+				if(type.IsValueType)
+				{
+					if(expect.Reversed)
+					{
+						throw new Exception(String.Format("The object {0} ({1}) exists", expect.TestObject, expect.Name));
+					}
+				}
+				else
+				{
+					if(expect.TestObject != null)
+					{
+						if(expect.Reversed)
+						{
+							throw new Exception(String.Format("The object {0} ({1}) exists", expect.TestObject, expect.Name));
+						}
+					}
+					else
+					{
+						if(!expect.Reversed)
+						{
+							throw new Exception(String.Format("The object {0} ({1}) doesn't exist", expect.TestObject, expect.Name));
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
